fb06229ab28f563309a3f627b6a31421bb96ad5e
Kamil Lelonek <squixy.sln@gmail.com>
2022-04-21 18:31:06 +0200

    Provide BlogPost mutation

---
+++ lib/sntx_graph/mutations/blog_posts.ex
@@ -0,0 +1,20 @@
+defmodule SntxGraph.Mutations.BlogPosts do
+  use Absinthe.Schema.Notation
+
+  import SntxWeb.Payload
+
+  alias Sntx.Models.BlogPost
+
+  object :blog_post_mutations do
+    field :blog_post_create, :blog_post_payload do
+      arg :input, non_null(:blog_post_create_input)
+
+      resolve(fn %{input: input} = _args, _ ->
+        case BlogPost.create(input) do
+          {:ok, blog_post} -> {:ok, blog_post}
+          error -> mutation_error_payload(error)
+        end
+      end)
+    end
+  end
+end
