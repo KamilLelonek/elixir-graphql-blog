60c25b24b2890e4490f0a2ee93469b246a27ded4
Kamil Lelonek <squixy.sln@gmail.com>
2022-04-21 13:38:12 +0200

    Add input object for creating a BlogPost

--- lib/sntx_graph/schema/blog_post.ex
+++ lib/sntx_graph/schema/blog_post.ex
@@ -11,4 +11,10 @@ defmodule SntxGraph.Schema.BlogPost do
     field :body, :string
     field :author, :user_public_account, resolve: dataloader(BasicDataloader)
   end
+
+  input_object :blog_post_create_input do
+    field :title, non_null(:string)
+    field :body, non_null(:string)
+    field :auhtor_id, non_null(:uuid4)
+  end
 end
